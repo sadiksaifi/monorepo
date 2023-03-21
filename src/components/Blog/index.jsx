e6ba88749529a8239c4a0e937f02c9bc16cd7fb2
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-03-22 02:29:27 +0530

    fix: changes in components

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -7,6 +7,7 @@ const Blog = () => {
     <div id='main' className="pt-24 md:pt-32 text-foreground">
       <h1 className="text-3xl font-extrabold">Posts</h1>
       <Dash className='my-2' />
+      <p className='mt-4 text-red-400 text-sm md:text-lg'>* Blog section is still in progress.</p>
       <div className="m-4">
         {data.map((item, index) => (
           <Card 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,16 @@
 import Dash from './Dash'
 import Button from './Button'
 import emailjs from '@emailjs/browser'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+
+const Required = () => <span className='text-red-500 mx-1'>*</span>
 
 const Contact = () => {
   const form = useRef()
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
   const sendEmail = (e) => {
     e.preventDefault()
-
     emailjs.sendForm('service_rd8i839', 'template_rrl7mbq', form.current, 'VWnoOerrYn4GOnO12')
       .then((result) => {
         console.log(result.text)
@@ -16,6 +18,7 @@ const Contact = () => {
         console.log(error.text)
       })
     e.target.reset()
+    setIsSubmitted(true)
   }
 
   return (
@@ -25,19 +28,20 @@ const Contact = () => {
       </h1>
       <Dash className='mx-auto my-6' />
       <p className="text-center text-sm text-altforeground">Feel free to Contact me by submitting the form below and I will get back to you as soon as possible</p>
-      <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-2 w-full md:w-[80%] mx-auto m-10 p-8 bg-altbackground rounded-xl'>
+      <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-2 w-full md:w-[80%] mx-auto m-10 p-6 md:p-8 bg-altbackground rounded-xl'>
         <div className="flex flex-col">
-          <label htmlFor='name' className='text-sm font-bold text-altforeground'>Name</label>
+          <label htmlFor='name' className='text-sm font-bold text-altforeground'>Name<Required /></label>
           <input type='text' name='user_name' className='rounded-lg p-4 my-2 text-sm bg-background text-foreground font-extrabold' placeholder="Enter Your Name" required />
         </div>
         <div className="flex flex-col">
-          <label htmlFor='email' className='text-sm font-bold text-altforeground'>Email</label>
+          <label htmlFor='email' className='text-sm font-bold text-altforeground'>Email<Required /></label>
           <input type='email' name='user_email' className='rounded-lg p-4 my-2 text-sm bg-background text-foreground font-extrabold' placeholder="Enter Your Email" required />
         </div>
         <div className="flex flex-col">
-          <label htmlFor='message' className='text-sm font-bold text-altforeground'>Message</label>
+          <label htmlFor='message' className='text-sm font-bold text-altforeground'>Message<Required /></label>
           <textarea name='user_message' id='message' className='rounded-lg p-4 my-2 h-52 text-sm bg-background resize-none text-foreground font-extrabold' placeholder="Enter Your Message" required />
         </div>
+        <p className={isSubmitted ? 'block' : 'block'}>Message Sent !</p>
         <Button content='Submit' type='submit' className='py-4 bg-primary text-foreground hover:bg-altforeground hover:text-background' />
       </form>
     </section>
