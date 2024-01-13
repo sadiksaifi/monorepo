a25d7495b593c842743cee7a7c6d7b8a2c9ccb6e
Sadik Saifi <thesadiksaifi@gmail.com>
2024-01-13 22:35:05 +0530

    feat: add vercel's analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({
             <Toaster />
           </>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
