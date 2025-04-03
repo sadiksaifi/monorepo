c7a1a10b38d0aee9bdc210a5aaf5b4b5ef47821d
Sadik Saifi <mail@sadiksaifi.dev>
2025-04-04 00:36:23 +0530

    chore: restructuring ui repo

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,21 +1,3 @@
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-
-const App: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
-  className,
-  ...props
-}) => {
-  return (
-    <div
-      className={cn(
-        "flex flex-col items-center justify-center min-h-svh",
-        className,
-      )}
-      {...props}
-    >
-      <Button>Click me</Button>
-    </div>
-  );
-};
-
-export default App;
+export function App() {
+  return <div>Hello World</div>;
+}
