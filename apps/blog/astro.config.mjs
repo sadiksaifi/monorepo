8e41479d7d210583f1bbe71c0d933275a9c92f1b
Sadik Saifi <mail@sadiksaifi.dev>
2025-04-05 00:11:30 +0530

    feat(blogs): add vercel analytics

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -4,13 +4,18 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   site: "https://blog.sadiksaifi.dev",
   integrations: [
