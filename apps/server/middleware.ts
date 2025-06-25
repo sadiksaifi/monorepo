795db66c9fd9ded547640da36631684a9d9ef837
Sadik Saifi <mail@sadiksaifi.dev>
2025-06-25 23:31:10 +0530

    fix(flat): cors issue of origin

--- apps/server/middleware.ts
+++ apps/server/middleware.ts
@@ -1,16 +1,22 @@
 import { NextResponse } from "next/server";
 
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ?? "http://localhost:5173";
+const CLIENT_ORIGINS = [process.env.CLIENT_ORIGIN, process.env.FLAT_FINDER_ORIGIN];
 
-export default async function middleware() {
+export default async function middleware(request: Request) {
+  const origin = request.headers.get("origin");
   const response = NextResponse.next();
+  let CLIENT_ORIGIN = "";
+
+  if (origin && CLIENT_ORIGINS.includes(origin)) {
+    CLIENT_ORIGIN = origin;
+  }
 
   response.headers.set("Access-Control-Allow-Credentials", "true");
   response.headers.set("Access-Control-Allow-Origin", CLIENT_ORIGIN);
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, b3, traceparent, x-uploadthing-version, x-uploadthing-package",
+    "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, b3, traceparent, ",
   );
 
   return response;
